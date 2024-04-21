d1b83363035bd4f3cb2d5a53cdfa167a4b79478c
VitorOdorico <ve743137@gmail.com>
2024-04-20 23:33:31 -0300

    add file an contents for.js

---
+++ js/for.js
@@ -0,0 +1,15 @@
+let contador = 1
+while(contador <= 10){
+    
+   // console.log(contador)
+    contador++
+}
+
+for(contador = 0; contador <= 10; contador++){
+    //console.log(contador)
+}
+
+const notasLista = [6.7,7.4,8.9,2.3,10];
+for(let i = 0; i < notasLista.length; i++){
+    console.log(`a nota é de : ${notasLista[i]}`)
+}
